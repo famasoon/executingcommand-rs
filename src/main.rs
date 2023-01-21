de0b0f0f4f81e23c0fcea87213764de0fa9aebc2
FAMASoon <famasoon@gmail.com>
2023-01-21 19:41:06 +0900

    execute command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::process::Command;
+
+fn executedcmd(cmd: &str) -> String {
+    let tmp = "/c ".to_owned();
+    let fullcmd = tmp + cmd;
+
+    let cmds: Vec<&str> = fullcmd.split(" ").collect();
+    let res = Command::new("cmd.exe").args(&cmds).output().unwrap();
+
+    let stdout = String::from_utf8_lossy(res.stdout.as_slice());
+    let stderr = String::from_utf8_lossy(res.stderr.as_slice());
+
+    if stdout.len() > 0 {
+        return stdout.to_string();
+    }
+    stderr.to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("[+] Usage: {} command", args[0]);
+    }
+    let res = executedcmd(&args[1]);
+    println!("{}", res)
 }
